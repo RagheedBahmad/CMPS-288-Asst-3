759b9141309a7ba3daaf7355cdd5c0ffeaad769e
RagheedBahmad <rlb01@aub.edu.lb>
2024-03-15 16:18:20 +0200

    Added player movement

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,18 +1,24 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    Camera camera = new Camera();
     // Start is called before the first frame update
     void Start()
     {
-        
+        camera = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        var pos = transform.position;
+        var mouse = camera.ScreenToWorldPoint(Input.mousePosition);
+        var x = Mathf.Clamp(mouse.x, -10, 10);
+        transform.position = new Vector2(x, pos.y);
     }
 }
